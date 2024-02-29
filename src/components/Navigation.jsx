160eb416db69ee6b7c827c38155a4865e3a58e0b
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2024-02-29 11:38:21 +0100

    added space at links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,30 +3,27 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <div className="flex justify-center text-black items-center h-screen">
-      <ul className="text-2xl space-y-10 text-center">
-        <Link to="/home">
+      <ul className="text-2xl text-center">
+        <Link className="block mb-4" to="/home">
           <li>
             <a>Home</a>
           </li>
         </Link>
-        <Link to="/search">
+        <Link className="block mb-4" to="/search">
           <li>
             <a>Search</a>
           </li>
         </Link>
-        <Link to="/schedule">
+        <Link className="block mb-4" to="/schedule">
           <li>
             <a>My Schedule</a>
           </li>
         </Link>
-        <Link to="/login">
+        <Link className="block mb-4" to="/login">
           <li>
             <a>Log in</a>
           </li>
         </Link>
-        <li>
-          <a>Log out</a>
-        </li>
       </ul>
     </div>
   );
